d98b6f78e4de8876e561445c7ce09257e78c967a
alfonso <highestlevel99yahoo.com>
2019-10-13 11:58:49 -0400

    Created different pages and fixed transitions

--- js/pageTransition.js
+++ js/pageTransition.js
@@ -1,54 +1,10 @@
+function transitionOut(panel){
 
-var currPanel = "Profile";
-var panels = ["Profile","About","Rutgers","Work","Projects","HackRU","ChiPhi"];
+  let currElement = document.getElementsByTagName("BODY")[0];
 
-function forwardTransition(newPanel){
-  let oldP = panels.indexOf(currPanel);
-  let newP = panels.indexOf(newPanel);
-  if(oldP < newP)
-    return true;
-  return false;
-}
+  currElement.className = "animated slideOutLeft";
 
-function transitionOut(newPanel){
-
-  if(newPanel == currPanel)
-    return;
-
-  let currElement = document.getElementById(currPanel);
-  let out = "";
-  if(forwardTransition(newPanel)){
-    out = "Left";
-  }else{
-    out = "Right";
-  }
-
-  currElement.className = "animated slideOut"+out;
-
-  console.log("test");
-
-  currElement.addEventListener('animationend', transitionIn(newPanel,out));
-}
-
-function transitionIn(newPanel,outTransition){
-
-  document.getElementById(currPanel).removeEventListener('animationend',transitionIn);
-
-  let inTransition = "";
-
-  if(outTransition == "Right"){
-    inTransition= "Left";
-  }else if(outTransition == "Left"){
-    inTransition= "Right";
-  }
-
-  let currElement = document.getElementById(currPanel);
-  currElement.style.display = "none";
-
-  let newElement = document.getElementById(newPanel);
-  console.log("current: "+currPanel);
-  newElement.style.display = "block"
-  newElement.className = "animated slideIn" + inTransition;
-  currPanel = newPanel;
-
+  currElement.addEventListener('animationend', function (){
+    window.location.href = panel;
+  });
 }
